5d06ecd24c03684e63c22ee6dbcc1c03d2a99fca
miv8229 <miv8229@gmail.com>
2016-02-19 21:21:40 +0600

    Upload sources

---
+++ drivers/input/misc/bst_sensor_common.c
@@ -0,0 +1,36 @@
+/*
+ * Created on Sep 19th, 2012, by guz4sgh
+ * Last modified: Sep 19th, 2012
+ *
+ * This software program is licensed subject to the GNU General Public License
+ * (GPL).Version 2,June 1991, available at http://www.fsf.org/copyleft/gpl.html
+
+ * (C) Copyright 2012 Bosch Sensortec GmbH
+ * All Rights Reserved
+ */
+
+#include "bst_sensor_common.h"
+
+#define MAX_AXIS_REMAP_TAB_SZ 8    /*P0~P7*/
+static const struct bosch_sensor_axis_remap
+bst_axis_remap_tab_dft[MAX_AXIS_REMAP_TAB_SZ] = {
+	/* src_x src_y src_z  sign_x  sign_y  sign_z */
+	{  0,    1,    2,     1,      1,      1 }, /* P0 */
+	{  1,    0,    2,     1,     -1,      1 }, /* P1 */
+	{  0,    1,    2,    -1,     -1,      1 }, /* P2 */
+	{  1,    0,    2,    -1,      1,      1 }, /* P3 */
+
+	{  0,    1,    2,    -1,      1,     -1 }, /* P4 */
+	{  1,    0,    2,    -1,     -1,     -1 }, /* P5 */
+	{  0,    1,    2,     1,     -1,     -1 }, /* P6 */
+	{  1,    0,    2,     1,      1,     -1 }, /* P7 */
+};
+
+
+
+
+
+
+
+
+MODULE_LICENSE("GPL");
